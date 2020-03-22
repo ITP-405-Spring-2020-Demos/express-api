6045bf131859f4612925e5f7e24d63bfbb5ac01f
David Tang <dtang@auditboard.com>
2020-03-21 17:23:39 -0700

    1.2 - Refactor to use custom module

--- index.js
+++ index.js
@@ -1,34 +1,16 @@
 const express = require('express');
-const sqlite3 = require('sqlite3').verbose();
+const Playlist = require('./models/playlist');
 
 const app = express();
 
-app.get('/api/playlists', (request, response) => {
-  const db = new sqlite3.Database('./chinook.db');
-
-  const sql = `
-    SELECT PlaylistId as id, Name as name
-    FROM playlists
-    ORDER BY name
-  `;
-  
-  db.all(sql, [], (error, rows) => {  
+app.get('/api/playlists', (request, response) => {  
+  Playlist.all((error, rows) => {  
     response.json(rows);
   });
-
-  db.close();
 });
 
 app.get('/api/playlists/:id', (request, response) => {
-  const db = new sqlite3.Database('./chinook.db');
-
-  const sql = `
-    SELECT PlaylistId as id, Name as name
-    FROM playlists
-    WHERE id = ?
-  `;
-  
-  db.get(sql, [request.params.id], (error, row) => {  
+  Playlist.get(request.params.id, (error, row) => {
     if (row) {
       response.json(row);
     } else {
@@ -37,8 +19,6 @@ app.get('/api/playlists/:id', (request, response) => {
       });
     }
   });
-
-  db.close();
 });
 
 app.listen(8000);
